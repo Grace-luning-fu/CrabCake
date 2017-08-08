fe3785971ef4ca55dc3fb4e7980c7653c4549ab1
grace-luning-fu <grace.luning.fu@gmail.com>
2017-08-08 00:38:18 -0400

    add more about bootstrap

--- src/main/java/com/grace/crabbyweb/controllers/MainController.java
+++ src/main/java/com/grace/crabbyweb/controllers/MainController.java
@@ -5,9 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping("/")
-    public String showReceipt() {
+    public String showRecipe() {
 
         return "crabcakepage";
     }
+
+
+    @GetMapping("/btdemo")
+    public String showRecipepp() {
+
+        return "btcrabcake";
+    }
+
+
+
 }
